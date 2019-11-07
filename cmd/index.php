1db6c6b3d5ffa7bd4b0913738e435ce596e10602
therealme1 <57095110+therealme1@users.noreply.github.com>
2019-11-07 21:30:08 +0800

    Add files via upload

--- cmd/index.php
+++ cmd/index.php
@@ -2,9 +2,11 @@
 if (isset($_POST['cmd'])) {
 	$cmd1=$_POST["cmd"];
 	$radioval=$_REQUEST["myradio"];
-	$rn1=$_POST["rename1"];
-	$rn2=$_POST["rename2"];
+	$rm1=$_POST["rename1"];
+	$rm2=$_POST["rename2"];
 }
+// zip cpmmand :- zip -r name.zip folder_name_to be zipped
+// unzip command:- unzip name.zip -d folder_name_where_to_be_zipoped
 ?>
 <html>
 <head>
@@ -16,22 +18,62 @@ if (isset($_POST['cmd'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-	
-</head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script>
+	  $(function() {
+	$("input[name='myradio']").click(function() {
+	if ($("#remo").is(":checked")) {
+  $("#text1").hide();
+	} else {
+	$("#text1").show();
+	}
+	});
+	}); 
+ 
+ 
+ $(function() {
+	$("input[name='myradio']").click(function() {
+	if ($("#rena").is(":checked")) {
+	$("#text1").hide();
+  $("#text2").show();
+	} else {
+	$("#text2").hide();
+	}
+	});
+	});
+	</script>
+	</head>
 
 <body>
 <div class="container" align="center">
 <form action="" method="post" class="form-group">
 <b>Select File or Folder:</b><br>
-<input type="radio" name="myradio" value="file" class="form-check-input"><b>Rclone File</b><br>
-<input type="radio" name="myradio" value="folder" class="form-check-input"><b>Rclone Folder</b><br>
-<input type="radio" name="myradio" value="custom" class="form-check-input"><b>Custom Command</b><br>
-<input type="radio" name="myradio" value="remove" class="form-check-input"><b>Remove all files from rapidleech</b><br>
-<input type="radio" name="myradio" value="rename" class="form-check-input" id="rename" ><b>Rename a File or Foler</b><br>
-<input type="text" class="form-control" name="cmd" placeholder="Enter your folder or file name here" id="enter"><br>
+<input type="radio" name="myradio" value="file"  ><b>Rclone File</b><br>
+<input type="radio" name="myradio" value="folder" ><b>Rclone Folder</b><br>
+<input type="radio" name="myradio" value="custom" ><b>Custom Command</b><br>
+<label for="remo">
+<input type="radio" name="myradio" value="rem" id="remo"><b>Remove</b><br>
+</label><br>
+<label for="rena">
+<input type="radio" name="myradio" value="ren" id="rena"><b>Rename</b><br>
+</label><br>
+<div id="text1"> 
+<b>Enter File/Folder Name:</b><br>
+<input type="text" class="form-control" name="cmd" placeholder="File/Folder or Commad"><br>
+</div>
 
-	<input type="submit" value="Exceute" class="btn btn-primary" name="execute"><br><br>
+
+<div id="text2" style="display: none">
+<b>Real Name to Modified name</b>
+<input type="text" class="form-control" name="rename1" placeholder="Real file Name">To
+<input type="text" class="form-control" name="rename2" placeholder="Modified name you want"><br>
+</div> 
+
+
+
+<input type="submit" value="Exceute" class="btn btn-primary" name="execute"><br><br>
 </form>
+
 	
 <?php if($radioval == "file") : ?>
 <?php		$cmd=shell_exec("rclone copy /app/files/" .$cmd1. " Telegram:Telegram"); ?>
@@ -64,10 +106,10 @@ if (isset($_POST['cmd'])) {
 <?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?>
         </pre>
 <?php endif; ?>
-	
-<?php if($radioval == "remove") : ?>
-<?php		$cmd=shell_exec("rm -rf ./files/* "); ?>
-		<?php if ($cmd) : ?>
+
+<?php elseif($radioval == "rem") :?>
+			<?php $cmd=shell_exec("rm -rf ./files/* "); ?>
+		<?php	if ($cmd) : ?>
 		<div class="pb-2 mt-4 mb-2">
             <h2> Output </h2>
         </div>
@@ -76,17 +118,17 @@ if (isset($_POST['cmd'])) {
         </pre>
 <?php endif; ?>
 
-<?php if($radioval == "rename") : ?>
-<?php		$cmd=shell_exec("mv " .$rn1 ." " .$rn2); ?>
-		<?php if ($cmd) : ?>
+<?php elseif($radioval == "ren"): ?>
+			<?php $cmd=shell_exec("mv ".$rm1." ".$rm2); ?>
+		<?php	if ($cmd) : ?>
 		<div class="pb-2 mt-4 mb-2">
             <h2> Output </h2>
         </div>
         <pre>
 <?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?>
         </pre>
-<?php endif; ?>	
-	
+<?php endif; ?>
+
 <?php elseif (!$cmd && $_SERVER['REQUEST_METHOD'] == 'POST') : 
 		{
 			echo "Error kindly contact @hackedyouagain";
